20360c8edcc567e29dcf03e05a13257cc276a408
mitsuru793 <mitsuru793@gmail.com>
2018-03-04 02:47:15 +0900

    fix git_shelf_test

--- test/git_shelf_test.rb
+++ test/git_shelf_test.rb
@@ -4,8 +4,4 @@ class GitShelfTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::GitShelf::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
