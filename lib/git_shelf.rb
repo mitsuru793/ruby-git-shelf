5d61e982d9cc57dd721b747b3441140ed9dfa0da
mitsuru793 <mitsuru793@gmail.com>
2018-05-16 00:29:39 +0900

    add Cli class

--- lib/git_shelf.rb
+++ lib/git_shelf.rb
@@ -1,6 +1,16 @@
+# stdlib
+require 'yaml'
+require 'shell'
+require 'pathname'
+
+# 3rd party
+require "thor"
+require 'awesome_print'
+
 require "git_shelf/version"
 require "git_shelf/repository"
 require "git_shelf/repositories"
 require "git_shelf/counter"
 require "git_shelf/repository_book"
 require "git_shelf/config"
+require "git_shelf/cli"
